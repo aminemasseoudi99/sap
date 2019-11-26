f9a91dad5a7a703e28a1c42840c66f0ad3ba94cb
aminemasseoudi99 <58235655+aminemasseoudi99@users.noreply.github.com>
2019-11-26 23:13:05 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>GeoConcept</title>
+    <link rel="stylesheet" href="Views/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .btn{ font: 18px SansSerif;}
+        body{ font: 14px sans-serif; }
+        .wrapper{ height: 500px; width: 500px; padding: 20px; left: 60%;top: 55%;
+            margin-left: -25%;
+            position: absolute;
+            margin-top: -20%;
+            border-width: 10px; border-style: double; border-color: #2b2a2a; }
+    </style>
+</head>
+<body >
+
+<div class="wrapper" >
+    <h1 style="margin: 30px; font-size: 50px;"><strong>GeoConcept</strong></h1>
+    <h2 style="margin-left: 35px;top: 20%"> Plateforme de test :</h2>
+    <a href="back/splite/la solution medicale/index.html" class="btn btn-success btn-lg " style="margin: 30px;width: 380px;font-family: Helvetica;font-size: 50px;" ><i class="fa fa-user"></i> Client</a><br>
+    <a href="back/splite/views/index.php" class="btn btn-primary btn-lg" style="margin: 30px;width: 380px;font-family: Helvetica;font-size: 50px;" ><i class="fa fa-user-secret "></i> Admin</a>
+</div>
+</body>
+<script src="Views/js/vendor/jquery-1.12.4.min.js"></script>
+<!-- bootstrap JS
+    ============================================ -->
+<script src="Views/js/bootstrap.min.js"></script>
+</html>
